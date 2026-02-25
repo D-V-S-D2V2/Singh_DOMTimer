42f974b3c8535088c163ad2cc2264c80d439c734
Darsh Pratap Singh <dpsingh1@my.waketech.edu>
2026-02-25 14:02:51 -0500

    im done

--- js.js
+++ js.js
@@ -1,33 +1,33 @@
-let startTime = document.getElementById("startTime");
-let endTime = document.getElementById("endTime");
-let reset = document.getElementById("reset");
 let text = document.getElementById("timer");
-let for1 = document.getElementById("TimerForm");
-let for3 = document.getElementById("TimerForm1");
-let for2 = document.getElementById("TimerForm2");
-
+let startBtn = document.getElementById("startTimer");
+let stopBtn = document.getElementById("stopTimer");
+let resetBtn = document.getElementById("resetTimer");
 window.count = 0;
-for1.addEventListener("click", function(event) {
 
+startBtn.addEventListener("click", function(event) {
     event.preventDefault();
-    
+    start();
+});
+function start() {
+    console.log(window.count); 
+    text.innerHTML = window.count;
+    window.count = window.count + 1;
     window.timer1 = setInterval(() => {
         console.log("Start3 Time");
         console.log(window.count); 
         text.innerHTML = window.count;
         window.count = window.count + 1;
-        //window.count = Number(window.count.toFixed(1))
     }, 1000 );
-});
+}
 
-for2.addEventListener("click", function() {
+resetBtn.addEventListener("click", reset);
+function reset() {
     window.count = 0;
     text.innerHTML = window.count;
     clearInterval(window.timer1);
-});
+}
 
-
-for3.addEventListener("click", function() {
-
+stopBtn.addEventListener("click", stop);
+function stop() {
     clearInterval(window.timer1);
-});
+}
